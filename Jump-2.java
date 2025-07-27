0c161548d8ebab9e2afd93bf95e968ec470bf57b
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-07-27 18:03:21 -0500

    Create Jump-2.java

---
+++ Jump-2.java
@@ -0,0 +1,18 @@
+// Time complexity - O(n)
+//space complexity -O(1)
+// solved on leetcode - yes
+// Faced any issues - No
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0, currentEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+}
